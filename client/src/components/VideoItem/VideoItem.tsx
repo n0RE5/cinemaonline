500fe5f825b02ad9e2a018674578dc72c1e4cd35
Your Name <you@example.com>
2023-08-20 13:05:11 +0300

    upd: updated video links

--- client/src/components/VideoItem/VideoItem.tsx
+++ client/src/components/VideoItem/VideoItem.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import classes from './VideoItem.module.scss';
 import { Link } from 'react-router-dom';
 import { IVideo } from '../../types/Interfaces';
+import { WatchRoute } from '../../tools/Consts';
+import { API_URL } from '../../api';
 
 interface VideoItemProps {
     video: IVideo
@@ -10,9 +12,11 @@ interface VideoItemProps {
 const VideoItem: React.FC<VideoItemProps> = ({ video }) => {
     return (
         <div className={classes.videoitem_w}>
-            <Link className={classes.videoitem} to={video.href}>
-                <div className={classes.videoitem__title}>{video.title}</div>
-                <img loading='lazy' src={video.img} alt={video.title} className={classes.videoitem__img} />
+            <Link className={classes.videoitem} to={WatchRoute + `?v=${video.href}`}>
+                <div className={classes.videoitem__title}>
+                    {video.title} ({video.year})
+                </div>
+                <img loading='lazy' src={API_URL + video.img} alt={video.title} className={classes.videoitem__img} />
             </Link>
         </div>
     );
