1be4b3d58c6799ed5e7969ebdb317c776684855c
Your Name <you@example.com>
2023-08-20 17:15:12 +0300

    upd: updated searchpage module

--- client/src/modules/SearchPageModule.tsx
+++ client/src/modules/SearchPageModule.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import classes from './SearchPage.module.scss';
-import VideoItemScroller from '../components/VideoItemScroller/VideoItemScroller';
 import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
-import { IBreadcrumb } from '../types/Interfaces';
+import { IBreadcrumb, IVideo } from '../types/Interfaces';
 import { useSearchParams } from 'react-router-dom';
+import VideoGrid from '../components/VideoGrid/VideoGrid';
+import Searchbar from '../components/Searchbar/Searchbar';
 
 interface SearchpageModuleProps {
-
+    videos: IVideo[]
 }
  
-const SearchpageModule: React.FC<SearchpageModuleProps> = () => {
+const SearchpageModule: React.FC<SearchpageModuleProps> = ({ videos }) => {
     const [params] = useSearchParams()
     const searchQuery = params.get('s')
     const [breadcrumbs] = useState<IBreadcrumb[]>([
@@ -17,18 +18,27 @@ const SearchpageModule: React.FC<SearchpageModuleProps> = () => {
             title: "Поиск по сайту"
         }
     ])
-    
+
     return (
-        <div>
-            <div className={classes.searchpage_header}>
-                <VideoItemScroller href='' title='Новинки кино' videos={[]} />
-            </div>
+        <div className={classes.searchpage}>
             <Breadcrumb breadcrumbs={breadcrumbs} />
             <div className={classes.searchpage_main}>
                 <h1 className={classes.searchpage_main__title}>Поиск по сайту</h1>
+                <div className={classes.searchpage_searchbar}>
+                    <Searchbar />
+                </div>
                 <div className={classes.searchpage_yellowbox}>
                     <div className={classes.searchpage_yellowbox__title}>Информация</div>
-                    <div className={classes.searchpage_yellowbox__query}>По запросу "{searchQuery}" найдено:</div>
+                    <div className={classes.searchpage_yellowbox__query}>
+                        {videos.length
+                            ? `По запросу "${searchQuery}" найдено:`
+                            : `-!- По запросу "${searchQuery}" ничего не найдено. Попробуйте изменить параметры или детали запроса. `
+                        }
+
+                    </div>
+                </div>
+                <div className={classes.searchpage_videogrid}>
+                    <VideoGrid videos={videos} />
                 </div>
             </div>
         </div>
