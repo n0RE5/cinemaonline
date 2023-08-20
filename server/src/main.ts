8709fa6ae27678800f7af6afec02551c654e55b5
Your Name <you@example.com>
2023-08-20 21:24:29 +0300

    upd: enabled cirs

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from "./app.module"
 
 const start = async () => {
     const PORT = process.env.PORT || 5000
-    const app = await NestFactory.create(AppModule, {rawBody: true, cors: true, bodyParser: true})
-
+    const app = await NestFactory.create(AppModule, {rawBody: true, bodyParser: true})
+    app.enableCors({
+        origin:`${process.env.CLIENT_URL}`,
+        credentials: true,
+        methods:'GET,PUT,POST,DELETE',
+        exposedHeaders: 'x-total-count'
+    });
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
 
