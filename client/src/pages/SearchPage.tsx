f451ac23dfd109f17113044b3d81ef1c9d9dbc73
Your Name <you@example.com>
2023-08-20 17:15:37 +0300

    upd: added search page logic

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
-import { IBreadcrumb } from '../types/Interfaces';
+import React, { useEffect, useState } from 'react';
+import { IBreadcrumb, IVideo } from '../types/Interfaces';
 import { useSearchParams } from 'react-router-dom';
 import SearchPageModule from '../modules/SearchPageModule';
+import { useFetching } from '../hooks/useFetching';
+import { searchVideos } from '../api/videoAPI';
+import { getPageCount } from '../tools/Pages';
  
 const SearchPage = () => {
     const [params] = useSearchParams()
-    const searchQuery = params.get('s')
+    const [videos, setVideos] = useState<IVideo[]>([])
+    const [totalPages, setTotalPages] = useState<number>(0)
+    const [limit, setLimit] = useState<number>(18)
+    const [page, setPage] = useState<number>(1)
+    const searchQuery = params.get('s') || ''
     
+    const [search, isLoading] = useFetching(async() => {
+        const response = await searchVideos(searchQuery, limit, page)
+        setVideos(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
+    })
+
+    useEffect(() => {
+        search()
+    }, [searchQuery, page])
+
     return (
         <>
-            <SearchPageModule />
+            <SearchPageModule videos={videos} />
         </>
     );
 }
