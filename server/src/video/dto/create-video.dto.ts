4bc479358c9c035781177a110875f11c96099844
Your Name <you@example.com>
2023-08-20 13:10:19 +0300

    add: added create-video dto

---
+++ server/src/video/dto/create-video.dto.ts
@@ -0,0 +1,14 @@
+export class CreateVideoDto {
+    href: string
+    img: string
+    video: string
+    title: string
+    country: string
+    genre: string
+    year: number
+    type: string
+    translate: string
+    quality: string
+    description: string
+    director: string
+}
