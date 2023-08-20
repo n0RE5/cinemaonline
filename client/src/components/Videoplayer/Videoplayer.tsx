7cae138aa728640cd01b279fa1ba4f68c7ec61a4
Your Name <you@example.com>
2023-08-20 13:05:33 +0300

    upd: added videoplayer

--- client/src/components/Videoplayer/Videoplayer.tsx
+++ client/src/components/Videoplayer/Videoplayer.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import classes from './Videoplayer.module.scss';
+import { IVideo } from '../../types/Interfaces';
+import { API_URL } from '../../api';
 
 interface VideoplayerProps {
- 
+    video: IVideo
 }
  
-const Videoplayer: React.FC<VideoplayerProps> = () => {
+const Videoplayer: React.FC<VideoplayerProps> = ({ video }) => {
     return (
         <div className={classes.videoplayer}>
-            <iframe 
-                src="https://vid1692175445672.vb17123filippaaniketos.pw/movie/3dd3074b9767f9393a85079abda1f2fb/iframe?d=lordfilm7.link" 
-                style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}} 
-                frameBorder="0" 
-                loading="lazy"
-            />
+            <video controlsList='nodownload' controls className={classes.videoplayer__player} src={API_URL + video.video}>
+                <source type="video/mp4" src={API_URL + video.video} />
+            </video>
         </div>
     );
 }
