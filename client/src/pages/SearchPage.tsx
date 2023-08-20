3450bf1e2c009160b9e76745f42b85ccbcea01c7
Your Name <you@example.com>
2023-08-20 21:26:00 +0300

    upd: added search result pagination

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -4,21 +4,21 @@ import { useSearchParams } from 'react-router-dom';
 import SearchPageModule from '../modules/SearchPageModule';
 import { useFetching } from '../hooks/useFetching';
 import { searchVideos } from '../api/videoAPI';
-import { getPageCount } from '../tools/Pages';
+import Pagination from '../components/Pagination/Pagination';
+import { usePagination } from '../hooks/usePagination';
  
 const SearchPage = () => {
     const [params] = useSearchParams()
     const [videos, setVideos] = useState<IVideo[]>([])
-    const [totalPages, setTotalPages] = useState<number>(0)
-    const [limit, setLimit] = useState<number>(18)
-    const [page, setPage] = useState<number>(1)
+    const [totalCount, setTotalCount] = useState<number>(0)
+    const {totalPages, page, setPage, limit, pagesArray} = usePagination(totalCount, 18)
     const searchQuery = params.get('s') || ''
     
     const [search, isLoading] = useFetching(async() => {
         const response = await searchVideos(searchQuery, limit, page)
         setVideos(response.data)
-        const totalCount = response.headers['x-total-count']
-        setTotalPages(getPageCount(totalCount, limit))
+        const totalVideoCount = response.headers['x-total-count']
+        setTotalCount(totalVideoCount)
     })
 
     useEffect(() => {
@@ -28,6 +28,7 @@ const SearchPage = () => {
     return (
         <>
             <SearchPageModule videos={videos} />
+            <Pagination totalPages={totalPages} page={page} pagesArray={pagesArray} setPage={setPage} />
         </>
     );
 }
