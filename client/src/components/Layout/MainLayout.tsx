d4a9f2ff9df0ed80d6aea0a5a97157234ffefa48
Your Name <you@example.com>
2023-08-20 13:04:07 +0300

    upd: updated layout

--- client/src/components/Layout/MainLayout.tsx
+++ client/src/components/Layout/MainLayout.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import classes from './MainLayout.module.scss';
 import Banner from '../Banner/Banner';
 import banner from '../../assets/banner.jpg'
 import SidebarModal from '../SidebarModal/SidebarModal';
+import VideoItemScroller from '../VideoItemScroller/VideoItemScroller';
+import { FilterRoute } from '../../tools/Consts';
+import { useFetching } from '../../hooks/useFetching';
+import { getNewestList } from '../../api/videoAPI';
+import { IVideo } from '../../types/Interfaces';
 
 interface LayoutProps {
     children: React.ReactNode
 }
  
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+    const [videos, setVideos] = useState<IVideo[]>([])
+    const [fetchVideos] = useFetching(async () => {
+        const response = await getNewestList(15)
+        setVideos(response)
+    })
+
+    useEffect(() => {
+        fetchVideos()
+    }, [])
+
     return (
         <>
             <Banner src={banner} />
             <SidebarModal />
             <Navbar />
             <div className={classes.layout}>
-                {children}
+                <div className="content">
+                    <div className="scroller">
+                        <VideoItemScroller href={FilterRoute + '?type=Фильм'} title='Новинки кино' videos={videos} />
+                    </div>
+                    {children}
+                </div>
             </div>
             <Footer />
         </>
