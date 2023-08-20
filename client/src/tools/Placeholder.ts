24aef2a098b8f6a72f838d7ea5da61fc6f991e17
Your Name <you@example.com>
2023-08-20 13:09:20 +0300

    add: added placeholder

---
+++ client/src/tools/Placeholder.ts
@@ -0,0 +1,16 @@
+import { IVideo } from "../types/Interfaces";
+
+export const videoPlaceholder: IVideo = {
+    href: "string",
+    img: "string",
+    video: "string",
+    title: "string",
+    country: "string",
+    genre: "",
+    year: 2023,
+    type: "Фильм",
+    translate: "string",
+    quality: "string",
+    description: "string",
+    director: "string"
+}
