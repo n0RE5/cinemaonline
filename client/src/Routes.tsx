3aaa6ba43d70a96c80ba618d53826574289a9e78
Your Name <you@example.com>
2023-08-20 13:01:59 +0300

    upd: added error route

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,9 +1,11 @@
+import { Navigate } from "react-router-dom";
 import CopyrightPage from "./pages/CopyrightPage";
 import FilterPage from "./pages/FilterPage";
 import MainPage from "./pages/MainPage";
 import SearchPage from "./pages/SearchPage";
 import VideoPage from "./pages/VideoPage";
-import { CopyrightRoute, DefaultRoute, FilterRoute, SearchRoute, WatchRoute } from "./tools/Consts";
+import { CopyrightRoute, DefaultRoute, ErrorRoute, FilterRoute, SearchRoute, WatchRoute } from "./tools/Consts";
+import Page404 from "./pages/Page404";
 
 export const publicRoutes = [
     {
@@ -19,11 +21,19 @@ export const publicRoutes = [
         element: <FilterPage />
     },
     {
-        path: WatchRoute, //+ '/:uuid',
+        path: WatchRoute,
         element: <VideoPage />
     },
     {
         path: SearchRoute, 
         element: <SearchPage />
+    },
+    {
+        path: ErrorRoute,
+        element: <Page404 />
+    },
+    {
+        path: '*',
+        element: <Navigate to={ErrorRoute} replace/>
     }
 ]
