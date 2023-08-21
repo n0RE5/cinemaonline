5d63941398c976a16f9043f0a87b5a497d4dc94f
Your Name <you@example.com>
2023-08-21 13:20:47 +0300

    upd: added react keys for list

--- client/src/components/Breadcrumb/Breadcrumb.tsx
+++ client/src/components/Breadcrumb/Breadcrumb.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Breadcrumb.module.scss';
 import { IBreadcrumb } from '../../types/Interfaces';
 import { Link } from 'react-router-dom';
+import { DefaultRoute } from '../../tools/Consts';
 
 interface BreadcrumbProps {
     breadcrumbs: IBreadcrumb[]
@@ -12,11 +13,11 @@ interface BreadcrumbProps {
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ breadcrumbs }) => {
     return (
         <div className={classes.breadcrumb}>
-            <span className={classes.breadcrumb__text}>FILMPRO</span>
+            <Link to={DefaultRoute} className={classes.breadcrumb__text}>FILMPRO</Link>
             {breadcrumbs.map((breadcrumb, index) =>
                 breadcrumb.href  
-                ? <Link className={classes.breadcrumb__link} to={breadcrumb.href}>
-                    <span key={index}> » {breadcrumb.title}</span>
+                ? <Link key={index} className={classes.breadcrumb__link} to={breadcrumb.href}>
+                    <span> » {breadcrumb.title}</span>
                   </Link>
                 : <span key={index}> » {breadcrumb.title}</span>  
             )}
