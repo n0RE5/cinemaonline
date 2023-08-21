d00395285e045317e13aa8bc2d61bb02e3221ab8
Your Name <you@example.com>
2023-08-21 16:25:33 +0300

    upd: added mainpage logic

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,12 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './MainPage.module.scss'; 
 import VideoItemScroller from '../components/VideoItemScroller/VideoItemScroller';
+import { useFetching } from '../hooks/useFetching';
+import { getAll } from '../api/videoAPI';
+import { usePagination } from '../hooks/usePagination';
+import { IVideo } from '../types/Interfaces';
+import MainPageModule from '../modules/MainPageModule';
+import Pagination from '../components/Pagination/Pagination';
 
 const MainPage = () => {
+    const [videos, setVideos] = useState<IVideo[]>([])
+    const [totalCount, setTotalCount] = useState<number>(0)
+    const {totalPages, page, setPage, limit, pagesArray} = usePagination(totalCount, 30)
+
+    const [search] = useFetching(async () => {
+        const response = await getAll(limit, page)
+        setVideos(response.data)
+        const totalVideoCount = response.headers['x-total-count']
+        setTotalCount(totalVideoCount)
+    })
+
+    useEffect(() => {
+        search()
+    }, [page])
+
     return (
-        <div className='content'>
-            <VideoItemScroller href='' title='Новинки кино' videos={[]} />
-        </div>
+        <>
+            <MainPageModule videos={videos} />
+            <Pagination setPage={setPage} page={page} pagesArray={pagesArray} totalPages={totalPages} />
+        </>
     );
 }
  
