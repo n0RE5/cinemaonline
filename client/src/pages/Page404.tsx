c56d96f5f1e8ac2d328d9828213adfd689888414
Your Name <you@example.com>
2023-08-21 13:22:11 +0300

    upd: updated page404

---
+++ client/src/pages/Page404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page404Module from '../modules/Page404Module';
+
+const Page404 = () => {
+    return (
+        <>
+            <Page404Module />
+        </>
+    );
+}
+ 
+export default Page404;
